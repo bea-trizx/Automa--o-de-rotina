ef582bfb1686fbd1229b420fed298c74c1a127a2
kilsonnnfelip <124009462+kilsonnnfelip@users.noreply.github.com>
2023-04-27 15:35:41 -0300

    first commit

---
+++ automaçãojob.py
@@ -0,0 +1,26 @@
+# importando 
+import pyautogui as bot
+from selenium import webdriver
+import time as time
+
+
+bot.PAUSE=2
+bot.press("win")
+bot.write("google chrome")
+bot.press("enter")
+bot.click(x=457,y=384)
+
+bot.click(x=1009, y=655)
+bot.click(x=1280, y=33)
+bot.click(x=53, y=304)
+bot.click(x=576, y=679)
+bot.write("Bom dia galera")
+
+
+
+bot.click(x=329, y=20)
+bot.click(x=835, y=628)
+bot.click(x=639, y=21)
+bot.click(x=699, y=631)
+bot.click(x=930, y=23)
+bot.click(x=363, y=625)
---
+++ position.py
@@ -0,0 +1,8 @@
+import pyautogui as bot
+
+#Comando para descobrir a posição do mouse 
+bot.sleep(3)
+print(bot.position())
+
+#Posição da icone de mensagem 
+  #<function position at 0x00000239854FEE80>
